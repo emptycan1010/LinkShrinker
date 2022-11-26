988ef8f1d7c71259ca82559adb963c15c7860d56
sp8 <emptycan1010@gmail.com>
2022-11-27 00:54:34 +0900

    Initial commit

---
+++ main.go
@@ -0,0 +1,118 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
+	"time"
+)
+
+type Links struct {
+	Link       string `bson:"link"`
+	Createtime int64  `bson:"createtime"`
+	Name       string `bson:"name"`
+}
+
+type MakeSHRURLform struct {
+	Link          Links
+	ReturnChannel chan string
+}
+
+type Queryform struct {
+	Name          string `bson:"name"`
+	ReturnChannel chan []Links
+}
+
+var querychan = make(chan Queryform)
+var makeshrurlchan = make(chan MakeSHRURLform)
+
+func main() {
+	go makeShrURL()
+	go queryit()
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) { c.JSON(200, gin.H{"message": "pong"}) })
+	r.GET("/:s", letsgo)
+	r.POST("/make", letsmakeit)
+	r.Run()
+}
+
+func letsgo(c *gin.Context) {
+	q := Queryform{Name: c.Param("s"), ReturnChannel: make(chan []Links)}
+	querychan <- q
+	databases := <-q.ReturnChannel
+	c.JSON(200, gin.H{"message": databases})
+}
+
+func letsmakeit(c *gin.Context) {
+	m := MakeSHRURLform{Link: Links{Link: c.PostForm("link"), Createtime: time.Now().Unix(), Name: c.PostForm("name")}, ReturnChannel: make(chan string)}
+	makeshrurlchan <- m
+	c.JSON(200, gin.H{"message": <-m.ReturnChannel})
+}
+
+func makeShrURL() {
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx := context.Background()
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Disconnect(ctx)
+	collection := client.Database("link").Collection("arr")
+
+	// Db Connected
+	for {
+		q := <-makeshrurlchan
+		// Let's Check name already exists
+		qq := Queryform{Name: q.Link.Name, ReturnChannel: make(chan []Links)}
+		querychan <- qq
+		databases := <-qq.ReturnChannel
+		if q.Link.Name == "make" {
+			q.ReturnChannel <- "This name is not allowed"
+		} else {
+			if len(databases) == 0 {
+				_, err := collection.InsertOne(ctx, q.Link)
+				q.ReturnChannel <- "OK"
+				if err != nil {
+					log.Fatal(err)
+				}
+			} else {
+				q.ReturnChannel <- "Name already exists"
+			}
+		}
+	}
+}
+
+func queryit() {
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx := context.Background()
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Disconnect(ctx)
+	collection := client.Database("link").Collection("arr")
+
+	// DB Connected
+	for {
+		q := <-querychan
+
+		cur, err := collection.Find(ctx, bson.M{"name": q.Name})
+		if err != nil {
+			log.Fatal(err)
+		}
+		var databases []Links
+		cur.All(ctx, &databases)
+		fmt.Println(databases)
+		q.ReturnChannel <- databases
+	}
+}
