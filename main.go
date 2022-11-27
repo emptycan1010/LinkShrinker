92bfb871996b7f7390f9537c7e1cebe39b9f1646
sp8 <emptycan1010@gmail.com>
2022-11-27 21:57:11 +0900

    add random link gen

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"log"
+	"math/rand"
 	"time"
 )
 
@@ -34,7 +35,7 @@ func main() {
 	go makeShrURL()
 	go queryit()
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) { c.JSON(200, gin.H{"message": "pong"}) })
+	r.GET("/ping", func(c *gin.Context) { c.JSON(200, gin.H{"message": RandomString(8)}) })
 	r.GET("/:s", letsgo)
 	r.POST("/make", letsmakeit)
 	r.Run()
@@ -44,11 +45,14 @@ func letsgo(c *gin.Context) {
 	q := Queryform{Name: c.Param("s"), ReturnChannel: make(chan []Links)}
 	querychan <- q
 	databases := <-q.ReturnChannel
-	c.JSON(200, gin.H{"message": databases})
+	//c.JSON(200, gin.H{"message": databases})
+	c.Redirect(302, databases[0].Link)
 }
 
 func letsmakeit(c *gin.Context) {
-	m := MakeSHRURLform{Link: Links{Link: c.PostForm("link"), Createtime: time.Now().Unix(), Name: c.PostForm("name")}, ReturnChannel: make(chan string)}
+	//m := MakeSHRURLform{Link: Links{Link: c.PostForm("link"), Createtime: time.Now().Unix(), Name: c.PostForm("name")}, ReturnChannel: make(chan string)}
+	m := MakeSHRURLform{Link: Links{Link: c.PostForm("link"), Createtime: time.Now().Unix(), Name: RandomString(8)}, ReturnChannel: make(chan string)}
+
 	makeshrurlchan <- m
 	c.JSON(200, gin.H{"message": <-m.ReturnChannel})
 }
@@ -74,11 +78,11 @@ func makeShrURL() {
 		querychan <- qq
 		databases := <-qq.ReturnChannel
 		if q.Link.Name == "make" {
-			q.ReturnChannel <- "This name is not allowed"
+			q.ReturnChannel <- "This name is not allowed. Try again Please"
 		} else {
 			if len(databases) == 0 {
 				_, err := collection.InsertOne(ctx, q.Link)
-				q.ReturnChannel <- "OK"
+				q.ReturnChannel <- fmt.Sprintf("http://siro157.xyz:8080/%s", q.Link.Name)
 				if err != nil {
 					log.Fatal(err)
 				}
@@ -115,4 +119,14 @@ func queryit() {
 		fmt.Println(databases)
 		q.ReturnChannel <- databases
 	}
+}
+
+func RandomString(n int) string {
+	var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+
+	s := make([]rune, n)
+	for i := range s {
+		s[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(s)
 }
